cb6a18c20e9f68fdb8e4f1d7a08098ecd62dffcc
OriginalCube <lancemiraflorcube@gmail.com>
2023-05-07 23:56:27 +0800

    Prices Prediction

--- src/Main.jsx
+++ src/Main.jsx
@@ -2,19 +2,13 @@ import React from "react";
 import Options from "./Options";
 
 const Main = () => {
-  const DataCategory = ["Price", "Size", "Check Database"];
   const [id, setId] = React.useState(0);
+  const [price, setPrice] = React.useState(0);
 
   const onChangeId = (ev) => {
     setId(ev);
     console.log(ev);
   };
-
-  const CategoryText = (ev) => (
-    <div className="text-xl opacity-80 h-full w-full">
-      <p onClick={() => onChangeId(ev.id)}>{ev.name}</p>{" "}
-    </div>
-  );
 
   return (
     <div className="h-full w-full">
@@ -23,19 +17,20 @@ const Main = () => {
           <div className="h-1/2 w-5/6 m-auto mt-16">
             <p className="text-2xl font-bold opacity-90">Prediction Setting</p>
             <div className="relative top-3 left-8 h-1/3 w-5/6 flex flex-col">
-              {DataCategory.map((e, index) => (
-                <CategoryText
-                  key={index}
-                  name={e}
-                  id={index}
-                  onChangeId={onChangeId}
-                />
-              ))}
+              <div className="">
+                <p className="text-3xl font-light">Predict Price</p>
+                <div className="text-xl font-normal flex">
+                  <p className="w-full">Price :</p>
+                  <p className="w-full text-right">
+                    $ {price.toString().substring(0, 6)}
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
         <div className="h-full w-5/6 bg-gray-700">
-          <Options />
+          <Options setPrice={setPrice} />
         </div>
       </div>
     </div>
--- src/Options.jsx
+++ src/Options.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import Requirement from "./Requirement.json";
 import Typewriter from "typewriter-effect";
 
-const Options = () => {
+const Options = (props) => {
+  const mainScroll = React.useRef();
   const [id, setId] = React.useState(1);
   const [history, setHistory] = React.useState([]);
   const [textData, setTextData] = React.useState("");
@@ -14,6 +15,33 @@ const Options = () => {
     setHistory([...history]);
     setTextData("");
   };
+
+  const PricePrediction = (e) => {
+    let total = 0;
+    total =
+      -156.447 * e[0] +
+      0.9884 * e[1] +
+      27813.9149 * [2] +
+      3241.3418 * e[3] +
+      291.5121 * e[4] +
+      188.8234 * e[5] +
+      4780.9532 * e[6] +
+      10219.6727 * e[7] +
+      5794.3604 * e[8] +
+      46.2344 * e[9] -
+      1064930.6952;
+    return total;
+  };
+
+  React.useEffect(() => {
+    if (history.length === 11) {
+      props.setPrice(PricePrediction(history));
+    }
+  }, [history]);
+
+  React.useEffect(() => {
+    mainScroll.current.scrollTop = mainScroll.current.scrollHeight;
+  }, [id]);
 
   const AnyaInput = (ev) => (
     <div className="h-auto w-full">
@@ -28,18 +56,25 @@ const Options = () => {
           </div>
           <div style={{ width: "93%" }} className="h-full font-medium">
             {id - 1 === ev.index && textData.length === 0 ? (
-              <Typewriter
-                options={{
-                  strings: `${ev.question}`,
-                  autoStart: true,
-                  delay: 0.001,
-                  loop: false,
-                }}
-              />
+              <div className="text-xl mb-1">
+                <Typewriter
+                  options={{
+                    strings: `${ev.question}`,
+                    autoStart: true,
+                    delay: 0.001,
+                    loop: false,
+                  }}
+                />
+              </div>
             ) : (
-              <p>{ev.question}</p>
+              <p className="text-xl mb-1">{ev.question}</p>
             )}
-            <p className="text-sm opacity-80">{ev.label}</p>
+            {ev.image ? (
+              <div className="h-auto w-full">
+                <img className="h-auto w-full" src={ev.image} alt="" />
+              </div>
+            ) : null}
+            <p className="text-sm opacity-80 font-normal">{ev.label}</p>
           </div>
         </div>
       </div>
@@ -54,7 +89,7 @@ const Options = () => {
               />
             </div>
             <div style={{ width: "93%" }} className="h-full">
-              <p className="mt-5 font-lightbold">{history[ev.index]}</p>
+              <p className="mt-5 font-medium">{history[ev.index]}</p>
             </div>
           </div>
         </div>
@@ -64,11 +99,15 @@ const Options = () => {
 
   return (
     <div className="h-full w-full">
-      <div className="h-5/6 overflow-hidden mt-10 pt-16 m-auto flex flex-col overflow-y-auto">
-        {Requirement["Block"].slice(0, id).map((e, index) => (
+      <div
+        ref={mainScroll}
+        className="h-5/6 overflow-hidden mt-10 pt-16 m-auto flex flex-col overflow-y-auto"
+      >
+        {Requirement["Price"].slice(0, id).map((e, index) => (
           <AnyaInput
             index={index}
             key={index}
+            image={e.img}
             question={e.question}
             label={e.label}
           />
